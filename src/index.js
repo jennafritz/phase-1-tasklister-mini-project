e1a743a43243a9e2c16e7b37e6225c9e289213db
Jede <brahmajede@gmail.com>
2021-04-09 16:03:05 -0400

    delete success- tried others

--- src/index.js
+++ src/index.js
@@ -6,8 +6,23 @@ let taskList = document.querySelector("#tasks")
 
 theForm.addEventListener("submit", function(event) {
   event.preventDefault()
+    
   let userInput = event.target["new-task-description"].value
   let newTask = document.createElement("li")
-  newTask.innerText = userInput
-  taskList.appendChild(newTask)
+  let deleteButton = document.createElement("button")
+  let priorityList = document.createElement("select")
+  let priorityOptionHigh = document.createElement("option")
+  let priorityOptionMedium = document.createElement("option")
+  let priorityOptionLow = document.createElement("option")
+    priorityOptionHigh.innerText = "High"
+    priorityOptionMedium.innerText = "Medium"
+    priorityOptionLow.innerText = "Low"
+    deleteButton.innerText = "x"
+    newTask.innerText = userInput
+    taskList.appendChild(newTask)
+    newTask.appendChild(deleteButton)
+    deleteButton.addEventListener("click", function(){
+    newTask.remove()
+  })
+  
 })
