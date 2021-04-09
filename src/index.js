5a3e1002a0b184e26868c1fa942dfcc204cc5b53
jennafritz <jennafritz24@gmail.com>
2021-04-09 15:28:53 -0400

    basic deliverables completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
 });
+
+let theForm = document.querySelector("#create-task-form")
+let taskList = document.querySelector("#tasks")
+
+theForm.addEventListener("submit", function(event) {
+  event.preventDefault()
+  let userInput = event.target["new-task-description"].value
+  let newTask = document.createElement("li")
+  newTask.innerText = userInput
+  taskList.appendChild(newTask)
+})
